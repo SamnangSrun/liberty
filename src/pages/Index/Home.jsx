04c05fffe31ed49603495c482ece2c42af4d61a9
Sothea <officalsengchanthea@gmail.com>
2025-05-27 00:03:39 +0700

    Fix import path case for Categories.jsx

--- src/pages/Index/Home.jsx
+++ src/pages/Index/Home.jsx
@@ -1,26 +1,23 @@
-import React from 'react'
-import HeroHome from '../../Components/Index/HeroHome'
-
-import ServiceCard from "../../Components/Index/ServiceCard.jsx";
-import Categories from "../../Components/index/Categories.jsx";
-import Anime from "../../Components/Index/Anime-Films/Anime.jsx";
-import Khmer from "../../Components/Index/KhmerLegend/khmer.jsx";
-import Feedbacks from "../../Components/Index/Feedback.jsx";
-import Questions from "../../Components/Index/Questions.jsx";
+import HeroHome from "@/Components/Index/HeroHome";
+import ServiceCard from "@/Components/Index/ServiceCard.jsx";
+import Categories from "@/Components/Index/Categories.jsx";
+import Anime from "@/Components/Index/Anime-Films/Anime.jsx";
+import Khmer from "@/Components/Index/KhmerLegend/khmer.jsx";
+import Feedbacks from "@/Components/Index/Feedback.jsx";
+import Questions from "@/Components/Index/Questions.jsx";
 
 const Home = () => {
   return (
     <div>
-        <HeroHome/>
-        
-        <ServiceCard/>
-        <Anime/>
-        <Categories/>
-        <Khmer/>
-        <Feedbacks/>
-        <Questions/>
+      <HeroHome />
+      <ServiceCard />
+      <Anime />
+      <Categories />
+      <Khmer />
+      <Feedbacks />
+      <Questions />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

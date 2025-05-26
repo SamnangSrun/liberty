9e54d2d16ffc5219a93a5cfb33b072c084bdf4f1
Sothea <officalsengchanthea@gmail.com>
2025-05-27 00:07:49 +0700

    Fix import path casing for Categories.jsx

--- src/pages/Index/Home.jsx
+++ src/pages/Index/Home.jsx
@@ -1,10 +1,10 @@
-import HeroHome from "@/Components/Index/HeroHome";
-import ServiceCard from "@/Components/Index/ServiceCard.jsx";
-import Categories from "@/Components/Index/Categories.jsx";
-import Anime from "@/Components/Index/Anime-Films/Anime.jsx";
-import Khmer from "@/Components/Index/KhmerLegend/khmer.jsx";
-import Feedbacks from "@/Components/Index/Feedback.jsx";
-import Questions from "@/Components/Index/Questions.jsx";
+import HeroHome from "@/components/index/HeroHome";
+import ServiceCard from "@/components/index/ServiceCard.jsx";
+import Categories from "@/components/index/Categories.jsx";
+import Anime from "@/components/index/Anime-Films/Anime.jsx";
+import Khmer from "@/components/index/KhmerLegend/khmer.jsx";
+import Feedbacks from "@/components/index/Feedback.jsx";
+import Questions from "@/components/index/Questions.jsx";
 
 const Home = () => {
   return (

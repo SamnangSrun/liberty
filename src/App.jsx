7435bbe87d5c42c1ec40a904c31f517054d502c7
Sothea <officalsengchanthea@gmail.com>
2025-05-26 23:49:16 +0700

    push for try deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import Hero from "./Components/Index/Hero.jsx";
-import ServiceCard from "./Components/Index/ServiceCard.jsx";
-import Categories from "./Components/index/Categories.jsx";
-import Anime from "./Components/Index/Anime-Films/Anime.jsx";
-import Khmer from "./Components/Index/KhmerLegend/khmer.jsx";
-import Feedbacks from "./Components/Index/Feedback.jsx";
-import Questions from "./Components/Index/Questions.jsx";
-
+import Hero from "@/Components/Index/Hero.jsx";
+import ServiceCard from "@/Components/Index/ServiceCard.jsx";
+import Categories from "@/Components/index/Categories.jsx";
+import Anime from "@/Components/Index/Anime-Films/Anime.jsx";
+import Khmer from "@/Components/Index/KhmerLegend/khmer.jsx";
+import Feedbacks from "@/Components/Index/Feedback.jsx";
+import Questions from "@/Components/Index/Questions.jsx";
 
 function App() {
   return (
     <>
-      
-      
       <Hero />
       <ServiceCard />
       <Anime />
